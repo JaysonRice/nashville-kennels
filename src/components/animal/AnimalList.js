179c0e5ae3f4d32fc2688c1af723fd9c883c811a
root <root@DESKTOP-2KLJLBF.localdomain>
2020-04-21 17:03:20 -0400

    Animals now show their owner and which kennel they belong to.

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,14 +2,27 @@ import React, { useContext } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import Animal from "./Animal"
 import "./Animal.css"
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
 
 export default () => {
     const { animals } = useContext(AnimalContext)
+    const { locations } = useContext(LocationContext)
+    const { customers } = useContext(CustomerContext)
+
 
     return (
         <div className="animals">
         {
-            animals.map(anim => <Animal key={anim.id} animal={anim} />)
+            animals.map(animal => {
+                const owner = customers.find(c => c.id === animal.customerId)
+                const clinic = locations.find(l => l.id === animal.locationId)
+
+            return <Animal key={animal.id}
+                        animal={animal} 
+                        customer ={owner}
+                        location={clinic}/>
+        })
         }
         </div>
     )
