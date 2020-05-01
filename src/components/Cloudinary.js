3c881a59e438a49026d47ccfa4b871b20a519c22
root <root@DESKTOP-2KLJLBF.localdomain>
2020-05-01 17:03:21 -0400

    Simple picture uploader available.

--- src/components/Cloudinary.js
+++ src/components/Cloudinary.js
@@ -8,7 +8,7 @@ export const Cloudinary = () =>{
     const files = e.target.files
     const data = new FormData()
     data.append('file', files[0])
-    data.append('upload_preset', 'KennelTest')
+    data.append('upload_preset', 'PetsPlease')
     setLoading(true)
     const res = await fetch(
       '	https://api.cloudinary.com/v1_1/dawhgtkqk/image/upload',
